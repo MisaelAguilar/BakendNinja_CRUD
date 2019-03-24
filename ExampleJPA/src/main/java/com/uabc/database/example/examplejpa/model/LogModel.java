0332a923cb2a1157efff7d0da5ab484f9670bd52
MisaelAguilar <47407461+MisaelAguilar@users.noreply.github.com>
2019-03-23 21:22:01 -0700

    Add files via upload

---
+++ ExampleJPA/src/main/java/com/uabc/database/example/examplejpa/model/LogModel.java
@@ -0,0 +1,16 @@
+package com.uabc.database.example.examplejpa.model;
+
+import lombok.Data;
+
+@Data
+public class LogModel {
+    private int id;
+    private String date;
+    private String details;
+    private String username;
+    private String url;
+
+    public LogModel() {
+
+    }
+}
---
+++ ExampleJPA/src/main/java/com/uabc/database/example/examplejpa/model/UserRoleModel.java
@@ -0,0 +1,14 @@
+package com.uabc.database.example.examplejpa.model;
+
+import lombok.Data;
+
+@Data
+public class UserRoleModel {
+    private int userRoleId;
+    private String user;
+    private String role;
+
+    public UserRoleModel(){
+
+    }
+}
